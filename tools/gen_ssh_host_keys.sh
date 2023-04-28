d7fe5c1034cfbb5a9ecd4ee145caf7086cf577fa
Krystian Dużyński <krystian.duzynski@gmail.com>
2023-04-28 20:58:51 +0200

    added setting correct file mode for generated host files

--- tools/gen_ssh_host_keys.sh
+++ tools/gen_ssh_host_keys.sh
@@ -2,3 +2,9 @@ ssh-keygen -N "" -f ssh_host_private_key_dsa -t dsa -m pkcs8
 ssh-keygen -N "" -f ssh_host_private_key_rsa -t rsa -m pkcs8
 ssh-keygen -e -f ssh_host_private_key_rsa.pub -m pkcs8 > ssh_host_private_key_rsa.pub.tmp
 mv ssh_host_private_key_rsa.pub.tmp ssh_host_private_key_rsa.pub
+
+chmod 600 \
+  ssh_host_private_key_dsa \
+  ssh_host_private_key_dsa.pub \
+  ssh_host_private_key_rsa \
+  ssh_host_private_key_rsa.pub
